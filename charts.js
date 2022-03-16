9c34679885c6349169c797b791cfa7ac91cd97e3
Hina Midnight <hinamidnight@imac.myfiosgateway.com>
2022-03-16 18:04:10 -0400

    final

--- charts.js
+++ charts.js
@@ -3,9 +3,10 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("./static/data/samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
-
+    console.log("sampleNames")
+    console.log(sampleNames)
     sampleNames.forEach((sample) => {
       selector
         .append("option")
@@ -32,11 +33,13 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("./static/data/samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
+
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
+
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
 
@@ -56,46 +59,54 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-    d3.json("./static/data/samples.json").then((data) => {
+    d3.json("samples.json").then((data) => {
+      console.log(data);
       // 3. Create a variable that holds the samples array. 
       var allSamples = data.samples;
+
       // 4. Create a variable that filters the samples for the object with the desired sample number.
-      var samplearray = sample.Data.filter(sampleObj => sampleObj.id == sample);
+      var sampleArray = allSamples.filter(sampleObj => sampleObj.id == sample);
+
+      var metadataArray = data.metadata.filter(sampleObj => sampleObj.id == sample);
 
       //  5. Create a variable that holds the first sample in the array.
-      var sampleOne = sample.Array[0];
+      var sampleOne = sampleArray[0];
       console.log(sampleOne)
+
+      var metadata = metadataArray[0];
 
       // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
       var otuId = sampleOne.otu_ids;
-      var otu_labels = sampleFilters.otu_labels;
+      var otuLabel = sampleOne.otu_labels;
       var sampleValues = sampleOne.sample_values;
+
+      var washFreq = parseFloat(metadata.wfreq);
+      console.log(washFreq);
 
       // 7. Create the yticks for the bar chart.
       // Hint: Get the the top 10 otu_ids and map them in descending order  
       //  so the otu_ids with the most bacteria are last. 
 
       var yticks = otuId.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
-      var xticks = sampleValues.slice(0, 10).reverse();
 
-      console.log(ysticks);
-      console.log(sampleValue);
-
+      console.log(yticks);
+      console.log(sampleValues);
 
       // 8. Create the trace for the bar chart. 
       var barData = [
         {
           y: yticks,
-          x: sampleValues,
-          text: otu_labels,
+          x: sampleValues.slice(0, 10).reverse(),
+          text: otuLabel.slice(0, 10).reverse(),
           type: "bar",
-          orientation: "h",
+          orientation: "h"
         }
       ];
       // 9. Create the layout for the bar chart. 
 
       var barLayout = {
         title: `Top Ten OTU IDS ${sample}`,
+        margin: { t: 30, l: 150 }
         //xaxis: { title:"Sample Values"},
         //yaxis: {title: "OTU IDS"}
       };
@@ -104,43 +115,45 @@ function buildCharts(sample) {
       Plotly.newPlot("bar", barData, barLayout);
  
 // 1. Create the trace for the bubble chart.
-  var bubbleData = [
-    {
-        x: outId,
+  var bubbleData = [{
+        x: otuId,
         y: sampleValues,
         mode: 'markers',
-        text: otulabel,
+        text: otuLabel,
         marker: {
+          size: sampleValues,
           color: otuId,
-          colorscale: "Earth",
-          size: sampleValues
-      }
-    }
-  ];
+          colorscale: "Earth"
+        }
+    }];
+    console.log(bubbleData);
 
 // 2. Create the layout for the bubble chart.
   var bubbleLayout = {
       title: 'Bacteria Culture Per Sample',
-      xaxis: { title: "Sample Values" },
-      yaxis: { title: "OTU IDS" }
+      showlegend: false,
+      xaxis: { title: "OTU ID", automargin: true},
+      yaxis: { automargin: true},
+      hovermode: "closest"
   };
+console.log(bubbleLayout);
 
 // 3. Use Plotly to plot the data with the layout.
-  Plotly.newPlot("bubble", bubbleData);
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout, {responsive: true});
+
 
 // 4. Create the trace for the gauge chart.
   var gaugeData = [
     {
+      domain: { x: [0,1], y:[0,1] },
+      value: washFreq,
+      title: { text: "<b>Belly Button Washing Frequency</b> <br>Scrubs per Week" },
       type: "indicator",
       mode: "gauge+number+delta",
-      value: washFreq,
-      title: { text: "<b>Belly Button Washing Frequency</b> <br>Scrubs per Week", font: { size: 20 } },
       gauge: {
-        axis: { range: [null, 10], tickwidth: 1, tickcolor: "darkblue" },
+        axis: { range: [null, 10] },
         bar: { color: "black" },
         bgcolor: "white",
-        borderwidth: 2,
-        bordercolor: "gray",
         steps: [
           { range: [0, 2], color: "pink" },
           { range: [2, 4], color: "orange" },
@@ -148,23 +161,19 @@ function buildCharts(sample) {
           { range: [6, 8], color: "black" },
           { range: [8, 10], color: "navy" },
         ],
-
       }
     }
-
   ];
 
   // 5. Create the layout for the gauge chart.
   var gaugeLayout = {
-      margin: { t: 25, r: 25, l: 25, b: 25 },
-      paper_bgcolor: "white",
-      font: { color: "darkblue", family: "Arial" }
-
+      width: 500,
+      height: 425,
+      margin: {t: 0, b: gaugeLayout}
   };
 
     // 6. Use Plotly to plot the gauge data and layout.
-    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+  Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 
-// sample.json no longer available
 }
